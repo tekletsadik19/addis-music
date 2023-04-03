c224c843bd35d82027c9cd4568d3abea63a6d70b
tekletsadik21 <tekletsadikethiopia@gmail.com>
2023-04-03 21:55:49 +0300

    Added Important Auth Configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,20 @@ import { db } from "@/lib/db";
 import {PrismaAdapter} from "@next-auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 
+const getGoogleCredentials = ()=>{
+    const clientId = process.env.GOOGLE_CLIENT_ID;
+    const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+    if(!clientId || clientSecret?.length === 0){
+        throw new Error("No ClientID For Google Provider Set");
+    }
+    if(!clientSecret || clientId?.length === 0){
+        throw new Error("No ClientSecret For Google Provider Set");
+    }
+    return {clientId,clientSecret}
+}
+
+
 export const authOptions:NextAuthOptions = {
     adapter:PrismaAdapter(db),
     session:{
@@ -13,7 +27,42 @@ export const authOptions:NextAuthOptions = {
     },
     providers:[
         GoogleProvider({
-            
+            clientId:getGoogleCredentials().clientId,
+            clientSecret:getGoogleCredentials().clientSecret
         })
-    ]
+    ],
+    callbacks:{
+        async session({ token, session }) {
+        if (token) {
+            session.user.id = token.id
+            session.user.name = token.name
+            session.user.email = token.email
+            session.user.image = token.picture
+        }
+    
+        return session
+        },
+        async jwt({ token, user }) {
+          const dbUser = await db.user.findFirst({
+            where: {
+              email: token.email,
+            },
+          })
+    
+          if (!dbUser) {
+            token.id = user!.id
+            return token
+          }
+    
+          return {
+            id: dbUser.id,
+            name: dbUser.name,
+            email: dbUser.email,
+            picture: dbUser.image,
+          }
+        },
+        redirect() {
+          return '/dashboard'
+        },
+    },
 }
